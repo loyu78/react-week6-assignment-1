7ce3db123cccab0e9bde82d1cb42e8d9519c274b
seokho <loyu78@gmail.com>
2021-03-04 22:07:24 +0900

    임시저장 강의1번

---
+++ src/NotFoundPage.test.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import NotFoundPage
+from './NotFoundPage
+';
+
+test('NotFoundPage
+', () => {
+render( < NotFoundPage /
+    >
+);
+});
